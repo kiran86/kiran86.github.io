f08487da2167c5cc3e5bb440b1926073341c7f22
kiran86 <kiran.sankar.das@gmail.com>
2025-09-19 03:05:17 +0000

    Deploying to gh-pages from @ kiran86/kiran86.github.io@22356fd378ed1fd865f64adb53fc9aba79461a30 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -458,9 +458,9 @@ ninja.data = [{
           description: "a brief report on CCIs and children housed therein.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-zylentrix-data-analysis",
+          title: 'Zylentrix Data Analysis',
+          description: "deep dive into understanding student learning behaviors, course engagement, and feedback trends",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
